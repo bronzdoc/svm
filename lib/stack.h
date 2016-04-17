f1ea9e267db73355fcbb659e8f23e2d900141c39
bronzdoc <lsagastume1990@gmail.com>
2016-04-17 16:51:53 -0600

    Fix syntax error

--- lib/stack.h
+++ lib/stack.h
@@ -1,13 +1,15 @@
-#ifndef _SVM_STACK_H_
-#define _SVM_STACK_H_
-typedef struct {
-    int a_stack[1024];
+#ifndef __SVM_STACK_H__
+#define __SVM_STACK_H__
+#define STACK_MAX_SIZE 1024
+struct stack_tag {
+    int items[STACK_MAX_SIZE];
     int sp;
-} Stack;
+};
+typedef struct stack_tag Stack ;
 
-Stack *newStack();
-void push(*Stack stack, int val);
-int pop(*Stack stack);
-int peek(*Stack stack)
+Stack * newStack();
+void push(Stack *stack, int val);
+int pop(Stack *stack);
+int peek(Stack *stack);
 
-#ifndef
+#endif
