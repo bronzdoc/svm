e9834309fb3c656e1142bdbba4330f828c85ef13
bronzdoc <lsagastume1990@gmail.com>
2016-04-17 16:55:59 -0600

    Use our stack data structure as the vm stack

--- svm.c
+++ svm.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include "lib/stack.h"
 #define EXIT 0
 
 int fetch();
-void eval(int);
+void eval(Stack*, int);
 
 // Machine instructions
 typedef enum {
@@ -14,11 +15,11 @@ typedef enum {
 } InstructionSet;
 
 const int program[] = {
-        PSH, 5,
-        PSH, 6,
-        ADD,
-        POP,
-        HLT
+    PSH, 5,
+    PSH, 6,
+    ADD,
+    POP,
+    HLT
 };
 
 // Instruction pointer
@@ -27,18 +28,17 @@ int ip = 0;
 // Stack pointer
 int sp = -1;
 
-// VM Stack
-int stack[256];
-
 // VM state
 int running = 1;
 
 int
 main(int argc, char *argv[])
 {
+    // VM Stack
+    Stack *stack = newStack();
     while(running) {
         int instr = fetch();
-        eval(instr);
+        eval(stack, instr);
         ip++;
     }
     return EXIT;
@@ -51,7 +51,7 @@ fetch()
 }
 
 void
-eval(int instr)
+eval(Stack *stack, int instr)
 {
     switch(instr) {
         case HLT: {
@@ -59,19 +59,18 @@ eval(int instr)
             break;
         }
         case PSH: {
-            stack[++sp] = program[++ip];
+            push(stack, program[++ip]);
+            //stack[++sp] = program[++ip];
             break;
         }
         case ADD: {
-            int a = stack[sp--];
-            int b = stack[sp--];
-            int result = a + b;
-
-            stack[++sp] = result;
+            int a = pop(stack);
+            int b = pop(stack);
+            push(stack, a + b);
             break;
         }
         case POP: {
-            printf("=> %d\n", stack[sp--]);
+            printf("=> %d\n", pop(stack));
             break;
         }
     }
